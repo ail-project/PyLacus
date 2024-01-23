c51e09f9b826996d5e3fe14f0fa121210cfb62d5
Raphaël Vinot <raphael@vinot.info>
2024-01-23 16:53:20 +0100

    new: Add check busy

--- tests/test_web.py
+++ tests/test_web.py
@@ -13,8 +13,9 @@ class TestBasic(unittest.TestCase):
         self.client = PyLacus(root_url="http://127.0.0.1:7100")
 
     def test_up(self) -> None:
-        self.assertTrue(self.client.is_up)
         self.assertTrue(self.client.redis_up())
+        self.assertTrue(self.client.is_up)
+        self.assertFalse(self.client.is_busy())
 
     def test_submit(self) -> None:
         uuid = self.client.enqueue(url="circl.lu")
